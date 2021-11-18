84979f0f737fd8255ecda59e3285ed7ab8081726
asuka730 <oceanlvr@foxmail.com>
2021-11-18 23:09:31 +0800

    添加主页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,60 +1,31 @@
 <template>
-  <div class="dashboard-editor-container" />
+  <div>
+    <div class="dashboard-editor-container" />
+    <div class="container">
+      <img class="img-bg" src="./bg.png">
+      <div class="right-detail">
+        <div class="right-detail-title">
+          <img class="icon" src="./icon.png">
+          <h2>数据实时监控</h2>
+        </div>
+        <h1>实时数据分析洞察，辅助快速决策</h1>
+        <div class="right-detail-content">
+          提供基于物化视图的实时分析解决方案，建立“用数据说话、用数据管理、用数据决策、用数据创新”的大数据中心平台管理机制，并深度应用于公共服务与行业监管，做到即时感知数据变化，快速做出决策。
+        </div>
+        <a-button class="btn-enter" href="#/dash">立即进入</a-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-// import GithubCorner from '@/components/GithubCorner'
-// import PanelGroup from './components/PanelGroup'
-// import LineChart from './components/LineChart'
-// import RaddarChart from './components/RaddarChart'
-// import PieChart from './components/PieChart'
-// import BarChart from './components/BarChart'
-// import TransactionTable from './components/TransactionTable'
-// import TodoList from './components/TodoList'
-// import BoxCard from './components/BoxCard'
-
-const lineChartData = {
-  newVisitis: {
-    expectedData: [100, 120, 161, 134, 105, 160, 165],
-    actualData: [120, 82, 91, 154, 162, 140, 145]
-  },
-  messages: {
-    expectedData: [200, 192, 120, 144, 160, 130, 140],
-    actualData: [180, 160, 151, 106, 145, 150, 130]
-  },
-  purchases: {
-    expectedData: [80, 100, 121, 104, 105, 90, 100],
-    actualData: [120, 90, 100, 138, 142, 130, 130]
-  },
-  shoppings: {
-    expectedData: [130, 140, 141, 142, 145, 150, 160],
-    actualData: [120, 82, 91, 154, 162, 140, 130]
-  }
-}
-
 export default {
   name: 'DashboardAdmin',
-  components: {
-    // GithubCorner,
-    // PanelGroup,
-    // LineChart,
-    // RaddarChart,
-    // PieChart,
-    // BarChart,
-    // TransactionTable,
-    // TodoList,
-    // BoxCard
-  },
+  components: {},
   data() {
-    return {
-      lineChartData: lineChartData.newVisitis
-    }
+    return {}
   },
-  methods: {
-    handleSetLineChartData(type) {
-      this.lineChartData = lineChartData[type]
-    }
-  }
+  methods: {}
 }
 </script>
 
@@ -62,23 +33,43 @@ export default {
 .dashboard-editor-container {
   padding: 32px;
   background-color: rgb(240, 242, 245);
-  position: relative;
-
-  .github-corner {
-    position: absolute;
-    top: 0px;
-    border: 0;
-    right: 0;
+}
+.container {
+  display: flex;
+  flex-direction: row;
+  .img-bg {
+    width: 600px;
+    height: auto;
+    max-width: 100%;
   }
-
-  .chart-wrapper {
-    background: #fff;
-    padding: 16px 16px 0;
-    margin-bottom: 32px;
+  .right-detail {
+    margin: 90px 0 0 90px;
+    .icon {
+      width: 36px;
+      height: 36px;
+      margin-right: 16px;
+    }
+    .right-detail-title {
+      display: flex;
+      flex-direction: row;
+    }
+    .right-detail-content {
+      width: 500px;
+      font-size: 18px;
+      line-height: 27px;
+      color: #343741;
+    }
+    .btn-enter {
+      margin-top: 24px;
+      background-image: linear-gradient(317deg, rgb(134, 109, 255), rgb(77, 107, 255));
+      color: #fff;
+      align-items: center;
+      padding: 12px 48px;
+      height: 48px;
+    }
   }
 }
-
-@media (max-width:1024px) {
+@media (max-width: 1024px) {
   .chart-wrapper {
     padding: 8px;
   }
